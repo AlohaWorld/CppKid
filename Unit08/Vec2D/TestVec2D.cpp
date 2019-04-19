403993cd5ade4d5487f15893d0a4e8f90734f011
Yidong Cui <nathan.cui@gmail.com>
2019-04-19 16:58:17 +0800

    代码示例增加： 二元运算符重载 复合运算符重载 数组下标运算符重载 符号运算符重载 自增自减运算符重载 流操作运算符重载 类型转换运算符重载

--- Unit08/Vec2D/TestVec2D.cpp
+++ Unit08/Vec2D/TestVec2D.cpp
@@ -11,29 +11,43 @@ int main() {
   cout << "v1 = " << v1.toString() << endl;
   cout << "v2 = " << v2.toString() << endl;
   // 向量加法： 向量+向量，向量+数
-  Vec2D v3 = v1.add(v2);
-  Vec2D v4 = v3.add(10.0);
+  Vec2D v3 = v1 + v2;
+  Vec2D v4 = v3 + 10.0;
   cout << "v3 = " << v3.toString() << endl;
   cout << "v4 = " << v4.toString() << endl;
   // 向量减法，向量点积，向量数乘
-  Vec2D v5 = v2.subtract(v1);
-  double v6 = v2.dot(v1);
-  Vec2D v7 = v3.multiply(2.1);
+  Vec2D v5 = v2 - v1;
+  double v6 = v2 * v1;
+  Vec2D v7 = 2.1 * v3;
   cout << "v2 - v1 = " << v5.toString() << endl;
   cout << "v2 . v1 = " << v6 << endl;
-  cout << "v3 * 2.1 = " << v7.toString() << endl;
+  cout << "v3 * 2.1 = " << (v3 * 2.1).toString() << endl;
+  cout << "2.1 * v3 = " << v7.toString() << endl;
+
+  Vec2D va1{ 10, 12 } , va2{ 1, 2 };
+  cout << " va1 += va2 : " << (va1 += va2).toString() << endl;
+  cout << " va1 -= va2 : " << (va1 -= va2).toString() << endl;
   // 向量求负值
   Vec2D v8 = v2.negative();
-  cout << "-v2 = " << v8.toString() << endl;
+  cout << "-v2 = " << (-v2).toString() << endl;
   // 向量自增/自减
-  cout << "++v8 = " << v8.increase().toString() << endl;
-  cout << "--v2 = " << v2.decrease().toString() << endl;
+  cout << "++v8 = " << (++v8).toString() << endl;
+  cout << "v8++ = " << (v8++).toString() << endl;
+  cout << "v8 = " << v8.toString() << endl;
+  cout << "--v2 = " << (--v2).toString() << endl;
   // 读取或者修改向量元素
-  cout << "v1.x_ = " << v1.at(0) << endl;
-  cout << "v1.y_ = " << v1.at(1) << endl;
+  v1[ 0 ] = 31.1;
+  cout << "v1.x_ = " << v1[ 0 ] << endl;
+  cout << "v1.y_ = " << v1[ 1 ] << endl;
+  // v1[ 8 ] = 10.0;
   // 向量的长度magnitude 和角度direction
   cout << "v1.magnitude = " << v1.magnitude() << endl;
+  cout << "double(v1) = " << static_cast<double>(v1) << endl;
   cout << "v1.direction = " << v1.direction() << endl;
   // 比较两个向量
   cout << "v1 compare v2 : " << v1.compareTo(v2) << endl;
+  cout << "Please input a Vec2D:";
+  Vec2D v9{};
+  std::cin >> v9; // operator>>(std::cin, v9);
+  cout << v9 << endl;
 }
--- Unit08/Vec2D/Vec2D.cpp
+++ Unit08/Vec2D/Vec2D.cpp
@@ -20,7 +20,7 @@ std::string Vec2D::toString() {
 }
 
 // 向量加法
-Vec2D Vec2D::add(Vec2D secondVec2D) {
+Vec2D Vec2D::add(const Vec2D & secondVec2D) {
   return Vec2D(x_ + secondVec2D.x_ , y_ + secondVec2D.y_);
 }
 
@@ -28,8 +28,21 @@ Vec2D Vec2D::add(Vec2D secondVec2D) {
 Vec2D Vec2D::add(double numeral) {
   return Vec2D(this->x_ + numeral , this->y_ + numeral);
 }
+
+Vec2D Vec2D::operator+ (const Vec2D & secondVec2D) {
+  return this->add(secondVec2D);
+}
+Vec2D Vec2D::operator+ (const double numeral) {
+  return this->add(numeral);
+}
+
+Vec2D& Vec2D::operator += (const Vec2D & secondVec2D) { // v1 += v2;
+  x_ += secondVec2D.x_;
+  y_ += secondVec2D.y_;
+  return (*this);
+}
 // 向量减法
-Vec2D Vec2D::subtract(Vec2D secondVec2D) {
+Vec2D Vec2D::subtract(const Vec2D & secondVec2D) {
   return Vec2D(x_ - secondVec2D.x_ , y_ - secondVec2D.y_);
 }
 
@@ -37,9 +50,20 @@ Vec2D Vec2D::subtract(Vec2D secondVec2D) {
 Vec2D Vec2D::subtract(double numeral) {
   return Vec2D(x_ - numeral , y_ - numeral);
 }
+Vec2D Vec2D::operator -(const Vec2D & secondVec2D) {
+  return this->subtract(secondVec2D);
+}
+Vec2D Vec2D::operator -(double numeral) {
+  return this->subtract(numeral);
+}
 
+Vec2D& Vec2D::operator -= (const Vec2D & secondVec2D) {
+  x_ -= secondVec2D.x_;
+  y_ -= secondVec2D.y_;
+  return (*this);
+}
 // 向量点积
-double Vec2D::dot(Vec2D secondVec2D) {
+double Vec2D::dot(const Vec2D & secondVec2D) {
   return (x_ * secondVec2D.x_ + y_ * secondVec2D.y_);
 }
 
@@ -48,23 +72,52 @@ Vec2D Vec2D::multiply(double multiplier) {
   return Vec2D(x_ * multiplier , y_ * multiplier);
 }
 
+double Vec2D::operator *(const Vec2D & secondVec2D) {
+  return this->dot(secondVec2D);
+}
+Vec2D Vec2D::operator *(double multiplier) {
+  return this->multiply(multiplier);
+}
+
+Vec2D operator *(double multiplier , Vec2D vec2d) {
+  return vec2d.multiply(multiplier);
+}
 // 向量求负值
 Vec2D Vec2D::negative() {
   return Vec2D(-x_ , -y_);
 }
 
+Vec2D Vec2D::operator -() {
+  return Vec2D(-this->x_ , -this->y_);
+}
 // 向量自增1
 Vec2D& Vec2D::increase() {
   x_++; y_++;
   return (*this);
 }
 
+Vec2D& Vec2D::operator++() {
+  x_++; y_++;
+  return (*this);
+}
+Vec2D Vec2D::operator ++(int dummy) {
+  Vec2D temp{ *this };
+  x_++; y_++;
+  return temp;
+}
 // 向量自减1
 Vec2D& Vec2D::decrease() {
   x_--; y_--;
   return *this;
 }
-
+Vec2D& Vec2D::operator --() {
+  return this->decrease();
+}
+Vec2D Vec2D::operator --(int dummy) { // 纯右值
+  Vec2D temp(*this);
+  this->decrease();
+  return temp;
+}
 // 读取或者修改向量元素
 double& Vec2D::at(const int index) {
   if (0 == index)
@@ -74,10 +127,16 @@ double& Vec2D::at(const int index) {
   else
     throw std::out_of_range("at() only accept 1 or 2 as parameter");
 }
-
+double& Vec2D::operator [] (const int& index) {
+  return at(index);
+}
 // 求向量的范数（长度)
 double Vec2D::magnitude() {
   return sqrt(x_ * x_ + y_ * y_);
+}
+
+Vec2D::operator double() {
+  return this->magnitude();
 }
 
 // 求向量与x+轴的夹角
@@ -93,4 +152,13 @@ int Vec2D::compareTo(Vec2D secondVec2D) {
     return 0;
   else
     return (m1 > m2 ? 1 : -1);
+}
+
+std::ostream & operator <<(std::ostream & os , const Vec2D & v) { // cout << v1;
+  os << "(" << v.x_ << ", " << v.y_ << ")";
+  return os;
+}
+std::istream& operator >> (std::istream & is , Vec2D & v) {
+  is >> v.x_ >> v.y_;
+  return is;
 }
