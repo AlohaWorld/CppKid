1d3cda6f5ec78d9ab03048f0259a214598e30a41
liu <100673540+xuann111@users.noreply.github.com>
2022-08-01 15:41:23 +0800

    Update StackOfIntegers.h

--- Unit05/StackOfIntegers/StackOfIntegers.h
+++ Unit05/StackOfIntegers/StackOfIntegers.h
@@ -1,24 +1,18 @@
-#pragma once
-// »ŒŒÒ1£∫±‡–¥StackOfIntegers¿‡
-/**
--elements: int[100]
--size: int
-+StackOfIntegers()
-+empty(): bool
-+peek(): int
-+push(value: int): int
-+pop(): int
-+getSize(): int
- */
+import <iostream>;
+import <cassert>;
+
+export module module1;
+export class StackOfIntegers;
+
 class StackOfIntegers {
 private:
-  int elements[ 100 ];
-  int size{ 0 };
+	int elements[100];
+	int size{ 0 };
 public:
-  bool empty();
-  int peek();
-  int push(int value);
-  int pop();
-  int getSize();
-  StackOfIntegers();
+	bool empty();
+	int peek();
+	int push(int value);
+	int pop();
+	int getSize();
+	StackOfIntegers();
 };
