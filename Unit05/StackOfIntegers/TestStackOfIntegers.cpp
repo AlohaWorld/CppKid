07ab3a77ccb273b2664800b873227ad320def8bc
liu <100673540+xuann111@users.noreply.github.com>
2022-08-01 15:46:58 +0800

    Update TestStackOfIntegers.cpp

--- Unit05/StackOfIntegers/TestStackOfIntegers.cpp
+++ Unit05/StackOfIntegers/TestStackOfIntegers.cpp
@@ -1,20 +1,21 @@
-#include< iostream>
-#include <cassert>
-#include "StackOfIntegers.h"
-// ÈÎÎñ2£º´´½¨Stack¶ÔÏó£¬Õ¹Ê¾ÈëÕ»³öÕ»²Ù×÷
+import module1;
+
+import <iostream>;
+import <cassert>;
+
 int main() {
-  StackOfIntegers s1{};
-  for (int i = 0; i < 5; i++) { // µ× 1 2 3 4 5 ¶¥
-    s1.push(i + 1);
-  }
-  std::cout << "Stack size = " << s1.getSize() << std::endl;
-  std::cout << "Top element is: " << s1.peek() << std::endl;
-  const int size = s1.getSize();
-  for (int i = 0; i < size; i++) {
-    std::cout << s1.pop() << " ";
-  }
-  std::cout << std::endl;
-  std::cout << "Stack now is empty: " << s1.empty() << std::endl;
-  std::cin.get();
-  return 0;
+    StackOfIntegers s1{};
+    for (int i = 0; i < 5; i++) { // åº• 1 2 3 4 5 é¡¶
+        s1.push(i + 1);
+    }
+    std::cout << "Stack size = " << s1.getSize() << std::endl;
+    std::cout << "Top element is: " << s1.peek() << std::endl;
+    const int size = s1.getSize();
+    for (int i = 0; i < size; i++) {
+        std::cout << s1.pop() << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Stack now is empty: " << s1.empty() << std::endl;
+    std::cin.get();
+    return 0;
 }
