5a117ce6fc774739ad7ac5ec7dad7e30535580a6
liu <100673540+xuann111@users.noreply.github.com>
2022-08-01 18:17:10 +0800

    Update Circle.cpp

--- Unit06/Inheritance/Circle.cpp
+++ Unit06/Inheritance/Circle.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-#include "Circle.h"
+import module2;
+import <iostream>;
 
 Circle::Circle() {
-  radius = 1.0;
+    radius = 1.0;
 }
 
-Circle::Circle(double radius_ , Color color_ , bool filled_) : Shape{ color_, filled_ } {
-  radius = radius_;
+Circle::Circle(double radius_, Color color_, bool filled_) : Shape{ color_, filled_ } {
+    radius = radius_;
 }
 
 double Circle::getArea() {
-  return (3.14 * radius * radius);
+    return (3.14 * radius * radius);
 }
 
 double Circle::getRadius() const {
-  return radius;
+    return radius;
 }
 
 void Circle::setRadius(double radius) {
-  this->radius = radius;
+    this->radius = radius;
 }
