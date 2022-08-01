95f89e329e2d540d446a8b49e8bb9ddf619bb3a7
liu <100673540+xuann111@users.noreply.github.com>
2022-08-01 15:14:35 +0800

    Update CopyCtor.cpp

--- Unit05/CopyCtor/CopyCtor.cpp
+++ Unit05/CopyCtor/CopyCtor.cpp
@@ -1,22 +1,25 @@
-#include <iostream>
-#include "Square.h"
+import module1;
+
+import <iostream>;
+
 int Square::numberOfObjects = 0;
-// 任务2：在栈和堆上分别拷贝创建Square对象
+
+// 浠诲姟2锛氬湪鏍堝拰鍫嗕笂鍒嗗埆鎷疯礉鍒涘缓Square瀵硅薄
 int main() {
-  Square s1(10.0);
-  std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
+	Square s1(10.0);
+	std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
 
-  Square s2{ s1 };
-  std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
+	Square s2{ s1 };
+	std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
 
-  Square* s3 = new Square{ s1 };
-  std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
+	Square* s3 = new Square{ s1 };
+	std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
 
-  std::cout << "s3's area is: " << s3->getArea() << std::endl;
+	std::cout << "s3's area is: " << s3->getArea() << std::endl;
 
-  delete s3;
-  std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
+	delete s3;
+	std::cout << "Squares: " << Square::getNumberOfObjects() << std::endl;
 
-  std::cin.get();
-  return (0);
+	std::cin.get();
+	return (0);
 }
