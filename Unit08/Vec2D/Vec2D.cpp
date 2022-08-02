123c46b54ade4090b814608e85b227f55bd79d23
liu <100673540+xuann111@users.noreply.github.com>
2022-08-02 10:00:42 +0800

    Update Vec2D.cpp

--- Unit08/Vec2D/Vec2D.cpp
+++ Unit08/Vec2D/Vec2D.cpp
@@ -1,164 +1,167 @@
-#include "Vec2D.h"
+import module1;
+
+import <iostream>;
+import <string>;
 
 Vec2D::Vec2D() {
-  x_ = 0.0;
-  y_ = 0.0;
+    x_ = 0.0;
+    y_ = 0.0;
 }
 
-Vec2D::Vec2D(double x , double y) {
-  x_ = x;
-  y_ = y;
+Vec2D::Vec2D(double x, double y) {
+    x_ = x;
+    y_ = y;
 }
 
 Vec2D::~Vec2D() {
 }
 
-// ½«ÏòÁ¿×ª»»Îª×Ö·û´®ÐÎÊ½±íÊ¾
+// å°†å‘é‡è½¬æ¢ä¸ºå­—ç¬¦ä¸²å½¢å¼è¡¨ç¤º
 std::string Vec2D::toString() {
-  return std::string("(" + std::to_string(x_) + ", "
-                     + std::to_string(y_) + ")");
+    return std::string("(" + std::to_string(x_) + ", "
+        + std::to_string(y_) + ")");
 }
 
-// ÏòÁ¿¼Ó·¨
-Vec2D Vec2D::add(const Vec2D & secondVec2D) {
-  return Vec2D(x_ + secondVec2D.x_ , y_ + secondVec2D.y_);
+// å‘é‡åŠ æ³•
+Vec2D Vec2D::add(const Vec2D& secondVec2D) {
+    return Vec2D(x_ + secondVec2D.x_, y_ + secondVec2D.y_);
 }
 
-// ÏòÁ¿ºÍÊýÖµ¼Ó·¨
+// å‘é‡å’Œæ•°å€¼åŠ æ³•
 Vec2D Vec2D::add(double numeral) {
-  return Vec2D(this->x_ + numeral , this->y_ + numeral);
+    return Vec2D(this->x_ + numeral, this->y_ + numeral);
 }
 
-Vec2D Vec2D::operator+ (const Vec2D & secondVec2D) {
-  return this->add(secondVec2D);
+Vec2D Vec2D::operator+ (const Vec2D& secondVec2D) {
+    return this->add(secondVec2D);
 }
 Vec2D Vec2D::operator+ (const double numeral) {
-  return this->add(numeral);
+    return this->add(numeral);
 }
 
-Vec2D& Vec2D::operator += (const Vec2D & secondVec2D) { // v1 += v2;
-  x_ += secondVec2D.x_;
-  y_ += secondVec2D.y_;
-  return (*this);
+Vec2D& Vec2D::operator += (const Vec2D& secondVec2D) { // v1 += v2;
+    x_ += secondVec2D.x_;
+    y_ += secondVec2D.y_;
+    return (*this);
 }
-// ÏòÁ¿¼õ·¨
-Vec2D Vec2D::subtract(const Vec2D & secondVec2D) {
-  return Vec2D(x_ - secondVec2D.x_ , y_ - secondVec2D.y_);
+// å‘é‡å‡æ³•
+Vec2D Vec2D::subtract(const Vec2D& secondVec2D) {
+    return Vec2D(x_ - secondVec2D.x_, y_ - secondVec2D.y_);
 }
 
-// ÏòÁ¿¼õÊý
+// å‘é‡å‡æ•°
 Vec2D Vec2D::subtract(double numeral) {
-  return Vec2D(x_ - numeral , y_ - numeral);
+    return Vec2D(x_ - numeral, y_ - numeral);
 }
-Vec2D Vec2D::operator -(const Vec2D & secondVec2D) {
-  return this->subtract(secondVec2D);
+Vec2D Vec2D::operator -(const Vec2D& secondVec2D) {
+    return this->subtract(secondVec2D);
 }
 Vec2D Vec2D::operator -(double numeral) {
-  return this->subtract(numeral);
+    return this->subtract(numeral);
 }
 
-Vec2D& Vec2D::operator -= (const Vec2D & secondVec2D) {
-  x_ -= secondVec2D.x_;
-  y_ -= secondVec2D.y_;
-  return (*this);
+Vec2D& Vec2D::operator -= (const Vec2D& secondVec2D) {
+    x_ -= secondVec2D.x_;
+    y_ -= secondVec2D.y_;
+    return (*this);
 }
-// ÏòÁ¿µã»ý
-double Vec2D::dot(const Vec2D & secondVec2D) {
-  return (x_ * secondVec2D.x_ + y_ * secondVec2D.y_);
+// å‘é‡ç‚¹ç§¯
+double Vec2D::dot(const Vec2D& secondVec2D) {
+    return (x_ * secondVec2D.x_ + y_ * secondVec2D.y_);
 }
 
-// ÏòÁ¿Êý³Ë
+// å‘é‡æ•°ä¹˜
 Vec2D Vec2D::multiply(double multiplier) {
-  return Vec2D(x_ * multiplier , y_ * multiplier);
+    return Vec2D(x_ * multiplier, y_ * multiplier);
 }
 
-double Vec2D::operator *(const Vec2D & secondVec2D) {
-  return this->dot(secondVec2D);
+double Vec2D::operator *(const Vec2D& secondVec2D) {
+    return this->dot(secondVec2D);
 }
 Vec2D Vec2D::operator *(double multiplier) {
-  return this->multiply(multiplier);
+    return this->multiply(multiplier);
 }
 
-Vec2D operator *(double multiplier , Vec2D vec2d) {
-  return vec2d.multiply(multiplier);
+Vec2D operator *(double multiplier, Vec2D vec2d) {
+    return vec2d.multiply(multiplier);
 }
-// ÏòÁ¿Çó¸ºÖµ
+// å‘é‡æ±‚è´Ÿå€¼
 Vec2D Vec2D::negative() {
-  return Vec2D(-x_ , -y_);
+    return Vec2D(-x_, -y_);
 }
 
 Vec2D Vec2D::operator -() {
-  return Vec2D(-this->x_ , -this->y_);
+    return Vec2D(-this->x_, -this->y_);
 }
-// ÏòÁ¿×ÔÔö1
+// å‘é‡è‡ªå¢ž1
 Vec2D& Vec2D::increase() {
-  x_++; y_++;
-  return (*this);
+    x_++; y_++;
+    return (*this);
 }
 
 Vec2D& Vec2D::operator++() {
-  x_++; y_++;
-  return (*this);
+    x_++; y_++;
+    return (*this);
 }
 Vec2D Vec2D::operator ++(int dummy) {
-  Vec2D temp{ *this };
-  x_++; y_++;
-  return temp;
+    Vec2D temp{ *this };
+    x_++; y_++;
+    return temp;
 }
-// ÏòÁ¿×Ô¼õ1
+// å‘é‡è‡ªå‡1
 Vec2D& Vec2D::decrease() {
-  x_--; y_--;
-  return *this;
+    x_--; y_--;
+    return *this;
 }
 Vec2D& Vec2D::operator --() {
-  return this->decrease();
+    return this->decrease();
 }
-Vec2D Vec2D::operator --(int dummy) { // ´¿ÓÒÖµ
-  Vec2D temp(*this);
-  this->decrease();
-  return temp;
+Vec2D Vec2D::operator --(int dummy) { // çº¯å³å€¼
+    Vec2D temp(*this);
+    this->decrease();
+    return temp;
 }
-// ¶ÁÈ¡»òÕßÐÞ¸ÄÏòÁ¿ÔªËØ
+// è¯»å–æˆ–è€…ä¿®æ”¹å‘é‡å…ƒç´ 
 double& Vec2D::at(const int index) {
-  if (0 == index)
-    return x_;
-  else if (1 == index)
-    return y_;
-  else
-    throw std::out_of_range("at() only accept 1 or 2 as parameter");
+    if (0 == index)
+        return x_;
+    else if (1 == index)
+        return y_;
+    else
+        throw std::out_of_range("at() only accept 1 or 2 as parameter");
 }
 double& Vec2D::operator [] (const int& index) {
-  return at(index);
+    return at(index);
 }
-// ÇóÏòÁ¿µÄ·¶Êý£¨³¤¶È)
+// æ±‚å‘é‡çš„èŒƒæ•°ï¼ˆé•¿åº¦)
 double Vec2D::magnitude() {
-  return sqrt(x_ * x_ + y_ * y_);
+    return sqrt(x_ * x_ + y_ * y_);
 }
 
 Vec2D::operator double() {
-  return this->magnitude();
+    return this->magnitude();
 }
 
-// ÇóÏòÁ¿Óëx+ÖáµÄ¼Ð½Ç
+// æ±‚å‘é‡ä¸Žx+è½´çš„å¤¹è§’
 double Vec2D::direction() {
-  return atan(y_ / x_);
+    return atan(y_ / x_);
 }
 
-// ±È½ÏÁ½¸öÏòÁ¿µÄ³¤¶È¡£Èç¹ûfirstVec2DÐ¡ÓÚsecondVec2D£¬·µ»Ø-1£¬Èô´óÓÚÔò·µ»Ø1£¬ÈôÏàµÈÔò·µ»Ø0
+// æ¯”è¾ƒä¸¤ä¸ªå‘é‡çš„é•¿åº¦ã€‚å¦‚æžœfirstVec2Då°äºŽsecondVec2Dï¼Œè¿”å›ž-1ï¼Œè‹¥å¤§äºŽåˆ™è¿”å›ž1ï¼Œè‹¥ç›¸ç­‰åˆ™è¿”å›ž0
 int Vec2D::compareTo(Vec2D secondVec2D) {
-  double m1 = this->magnitude();
-  double m2 = secondVec2D.magnitude();
-  if (abs(m1 - m2) < 1e-10)
-    return 0;
-  else
-    return (m1 > m2 ? 1 : -1);
+    double m1 = this->magnitude();
+    double m2 = secondVec2D.magnitude();
+    if (abs(m1 - m2) < 1e-10)
+        return 0;
+    else
+        return (m1 > m2 ? 1 : -1);
 }
 
-std::ostream & operator <<(std::ostream & os , const Vec2D & v) { // cout << v1;
-  os << "(" << v.x_ << ", " << v.y_ << ")";
-  return os;
+std::ostream& operator <<(std::ostream& os, const Vec2D& v) { // cout << v1;
+    os << "(" << v.x_ << ", " << v.y_ << ")";
+    return os;
 }
-std::istream& operator >> (std::istream & is , Vec2D & v) {
-  is >> v.x_ >> v.y_;
-  return is;
+std::istream& operator >> (std::istream& is, Vec2D& v) {
+    is >> v.x_ >> v.y_;
+    return is;
 }
